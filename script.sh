ead4b747c22a95307c58ee727e06c417b9063368
ks-roushan <165149440+ks-roushan@users.noreply.github.com>
2024-05-19 13:29:27 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set your GitHub username
+GITHUB_USERNAME="ks-roushan"
+
+# Set the GitHub repository name
+GITHUB_REPOSITORY="se2001-23f1002240"
+
+# Export the variables
+export GITHUB_USERNAME
+export GITHUB_REPOSITORY
